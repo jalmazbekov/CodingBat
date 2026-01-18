f3d48fce84f515f76d2bb57d8676a2f0f87c8285
jalmazbekov <hazim.almazbekov@gmail.com>
2026-01-18 21:24:11 +0600

    последний коммиит

--- src/Joma/String1Part1.java
+++ src/Joma/String1Part1.java
@@ -230,6 +230,7 @@ public class String1Part1 {
         String first2 = str.substring(0, 2);
         String last2  = str.substring(str.length() - 2);
         return first2.equals(last2);
+
     }
 
 }
